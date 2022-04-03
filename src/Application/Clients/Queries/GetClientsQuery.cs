b72840d756620e9701970e68a2efecfd31b8562f
Guillaume Bodson <guillaume.bodson@contraste.com>
2022-04-03 11:33:50 +0200

    Add GetClientQuery and needed types

---
+++ src/Application/Clients/Queries/GetClientsQuery.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CleanArchitecture.Application.Clients.Modells;
+using CleanArchitecture.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Application.Clients.Queries;
+public class GetClientsQuery: IRequest<IEnumerable<ClientDto>>
+{
+
+}
+public class GetClientsQueryHandler : IRequestHandler<GetClientsQuery, IEnumerable<ClientDto>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetClientsQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    public async Task<IEnumerable<ClientDto>> Handle(GetClientsQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Clients.ProjectTo<ClientDto>(_mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+    }
+}
---
+++ src/WebUI/Controllers/ClientsController.cs
@@ -0,0 +1,12 @@
+﻿using CleanArchitecture.Application.Clients.Modells;
+using CleanArchitecture.Application.Clients.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.WebUI.Controllers;
+public class ClientsController : ApiControllerBase
+{
+    public async Task<IEnumerable<ClientDto>> GetClients()
+    {
+        return await Mediator.Send(new GetClientsQuery());
+    }
+}
