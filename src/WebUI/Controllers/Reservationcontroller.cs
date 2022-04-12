d978f6c4df1db52d55430536dd3e09c7609cc2f6
Guillaume Bodson <guillaume.bodson@contraste.com>
2022-04-12 21:51:29 +0200

    add reservation controller

---
+++ src/WebUI/Controllers/Reservationcontroller.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Application.Reservations.Commands;
+using CleanArchitecture.Application.Reservations.Models;
+using CleanArchitecture.Application.Reservations.Queries.GetClientReservations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.WebUI.Controllers;
+public class ReservationController : ApiControllerBase
+{
+    [HttpGet("Id")]
+    public async Task<IEnumerable<ReservationDto>> GetReservationsbyClientId(int clientId)
+    {
+        return await Mediator.Send(new GetClientReservationsQuery { ClientId = clientId });
+    }
+    [HttpPost()]
+    public async Task<ActionResult<bool>> ReserveVehicle(ReserveVehicleCommand command)
+    {
+        return await Mediator.Send(command);
+    }
+    [HttpGet("close")]
+    public async Task<ActionResult<CloseReservationDto>> CloseReservation(int reservationId, int NbKilometers = 0)
+    {
+        return await Mediator.Send(new CloseReservationCommand { NbKilometers = NbKilometers, ReservationId = reservationId });
+    }
+}
