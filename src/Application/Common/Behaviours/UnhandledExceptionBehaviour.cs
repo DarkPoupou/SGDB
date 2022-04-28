a0db96c780e9aa8968f0641e34cea7fd124b8b3b
Guillaume <bodson.guillaume@gmail.com>
2022-04-28 07:03:53 +0200

    ad sql exeption

--- src/Application/Common/Behaviours/UnhandledExceptionBehaviour.cs
+++ src/Application/Common/Behaviours/UnhandledExceptionBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using System.Data.SqlClient;
+using MediatR;
 using Microsoft.Extensions.Logging;
 
 namespace CleanArchitecture.Application.Common.Behaviours;
@@ -17,6 +18,25 @@ public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavio
         try
         {
             return await next();
+        }
+        catch(SqlException exception)
+        {
+            switch (exception.Number)
+            {
+                case 4060:
+                    _logger.LogCritical($"the Database specified does not exist on {exception.Server}");
+                    break;
+                case 18456:
+                    _logger.LogCritical($"Bad credentials");
+                    break;
+                case 487:
+                    _logger.LogCritical("Conflict");
+                    break;
+                default:
+                    _logger.LogCritical($"{exception.Number} -- {exception.Message}");
+                    break;
+            }
+            throw;
         }
         catch (Exception ex)
         {
